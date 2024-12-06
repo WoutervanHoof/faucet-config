77847ec7db52464042c16368e572da83c5a0c948
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2024-12-06 14:36:29 +0100

    newer version that was left on pi

--- switch_setup.sh
+++ switch_setup.sh
@@ -1,24 +1,28 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-device=$1
+ifname=$1
+controller_ip=$2
 
-if [ -z "$device" ]; then
-	read -p "Please provide interface name: " device
-fi
-
-if ! ip link show dev "$device"  ; then
-	echo "Device $device not found"
-	exit 1
-fi
-
-echo "Device $device"
-
-sudo ip link add dev veth-thread type veth
-sudo ip link set veth-thread up
+wpan_ips=$(ip -o addr show dev wpan0 | awk '{print $4}')
+gateway_ips=$(ip -o addr show dev "$ifname" | awk '{print $4}')
+default_ip=$(ip route show | grep 'default' | awk '{print $3}')
 
 sudo ovs-vsctl add-br br0 \
 	-- set bridge br0 other-config:datapath-id=0000000000000001 \
-	-- set bridge br0 fail_mode=secure \
-	-- add-port br0 veth-thread \
-	-- set interface veth-thread ofport_request=1 \
-	-- set-controller br0 tcp:10.42.0.1:6653 tcp:10.42.0.1:6654
+	-- set bridge br0 fail-mode=secure \
+	-- set-controller br0 tcp:"$controller_ip":6653 tcp:"$controller_ip":6654
+
+#for ip in $gateway_ips; do
+#	ip addr add "$ip" dev br0
+#done
+
+for ip in $wpan_ips; do
+	sudo ip addr add "$ip" dev br0
+done
+
+# sudo ip route append default via "$default_ip" dev br0
+
+sudo ovs-vsctl add-port br0 "$ifname" \
+	-- set interface "$ifname" ofport_request=1 \
+	-- add-port br0 wpan0 \
+	-- set interface wpan0 ofport_request=2
