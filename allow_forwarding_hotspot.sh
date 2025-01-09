aa13a3f4be1b46f735d3c0996c8250db998bac17
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-01-09 12:47:34 +0100

    fix: add ip6tables

--- allow_forwarding_hotspot.sh
+++ allow_forwarding_hotspot.sh
@@ -2,5 +2,8 @@
 
 # Allow forwarding things with destination of wlp3s0
 sudo iptables -A FORWARD -o wlp3s0 -j ACCEPT
+sudo ip6tables -A FORWARD -o wlp3s0 -j ACCEPT
+
 # Allow forwarding things comming from wlp
 sudo iptables -A FORWARD -i wlp3s0 -j ACCEPT
+sudo ip6tables -A FORWARD -i wlp3s0 -j ACCEPT
