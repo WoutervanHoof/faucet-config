6bd6a1f30e021f6030b4f1827edb3d20c7e522d9
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-01-23 15:13:50 +0100

    fix: add volume for rcp

--- run_docker.sh
+++ run_docker.sh
@@ -2,16 +2,37 @@
 
 set -euxo pipefail
 
-# Required for otbr-docker
-sudo modprobe ip6table_filter
+ORM_PREFIX="fd71:666b:b2e1:bfd9::"
+MANAGEMENT_IP="10.42.0.10"
+passphrase="mystify-vantage-deduct"
+NET_KEY="00112233445566778899aabbccddeeff"
 
-# Adjust /dev/ttyACM* in compose file
-if [[ ! -f /dev/ttyACM0 ]] ; then
-	dev_file=$(ls /dev/ttyACM* | head -n 1)
-	sed -i -r -e "s#/dev/ttyACM.#${dev_file}#" docker-compose.yml
-fi
+# # Required for otbr-docker
+# sudo modprobe ip6table_filter
 
-docker-compose up
+# # Adjust /dev/ttyACM* in compose file (a bit hacky, but oh well)
+# if [[ ! -f /dev/ttyACM0 ]] ; then
+# 	dev_file=$(ls /dev/ttyACM* | head -n 1)
+# 	sed -i -r -e "s#/dev/ttyACM.#${dev_file}#" docker-compose.yml
+# fi
 
+# docker-compose up
 
-curl -H "Content-Type: application/json" --request POST --data '{"networkKey":"00112233445566778899aabbccddeeff","prefix":"fd11:22::","defaultRoute":true,"extPanId":"1111111122222222","panId":"0x1234","passphrase":"j01Nme","channel":15,"networkName":"OpenThreadDemo"}' http://localhost:8080/form_network
+while true ; do
+	sleep 5
+
+	if  curl -H "Content-Type: application/json" --request POST --data '{
+		"networkKey":"'"${NET_KEY}"'",	
+		"prefix":"'"${ORM_PREFIX}"'",
+		"defaultRoute":true,
+		"extPanId":"1111111122222222",
+		"panId":"0x1234",
+		"passphrase":"'"${passphrase}"'",
+		"channel":15,
+		"networkName":"OpenThreadDemo"}' \
+	"http://${MANAGEMENT_IP}:8080/form_network" | grep -q "success" ; then
+		echo "Success"
+	fi
+
+	echo "sleeping then trying again"
+done
