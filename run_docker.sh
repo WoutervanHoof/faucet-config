6b8f35f90364cb0931ec1e1affa50d6dafd29c01
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-01-24 14:43:07 +0000

    feat: stop otbr-agent service before starting docker container

--- run_docker.sh
+++ run_docker.sh
@@ -5,10 +5,13 @@ set -euxo pipefail
 # Required for otbr-docker
 sudo modprobe ip6table_filter
 
-if [ ! -f /dev/ttyACM0 ] ; then 
-	echo "File /dev/ttyACM0 does not exist"
-	exit 1
-fi
+# If otbr-agent is still running, it will error
+sudo service otbr-agent stop
+
+#if [ ! -f /dev/ttyACM0 ] ; then 
+#	echo "File /dev/ttyACM0 does not exist"
+#	exit 1
+#fi
 
 docker container prune
 
