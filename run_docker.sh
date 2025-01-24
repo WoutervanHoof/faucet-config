e27e58d6e33a3c261df4c3de141d2e9811c84208
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-01-24 09:56:03 +0000

    fix: remove unused if

--- run_docker.sh
+++ run_docker.sh
@@ -6,10 +6,9 @@ set -euxo pipefail
 sudo modprobe ip6table_filter
 
 # Adjust /dev/ttyACM* in compose file (a bit hacky, but oh well)
-if [[ ! -f /dev/ttyACM0 ]] ; then
-	dev_file=$(ls /dev/ttyACM* | head -n 1)
-	sed -i -r -e "s#/dev/ttyACM.#${dev_file}#" docker-compose.yml
-fi
+dev_file=$(ls /dev/ttyACM* | head -n 1)
+sed -i -r -e "s#/dev/ttyACM.#${dev_file}#" docker-compose.yml
+
 
 docker container prune
 
