01033b58504c439516d36109d78be7aaff528ede
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-02-10 11:46:34 +0100

    fix: allow activate_thread to account for br number

--- activate_thread.sh
+++ activate_thread.sh
@@ -7,20 +7,23 @@ if [[ -z "$1" ]] ; then
     exit 1
 fi
 
+if [[ -z "$2" ]] ; then 
+    echo "Please provide the border router number"
+    exit 1
+fi
+
 set -u
 
 container_name="$1"
+NUMBER="$2"
 ORM_PREFIX="fd71:666b:b2e1:bfd9::"
 MANAGEMENT_IP="10.42.0.10"
-BR_IPV6="fdbe:8cb7:f64c:1::2"
+BR_IPV6="fdbe:8cb7:f64c:${NUMBER}::2"
 passphrase="mystify-vantage-deduct"
 NET_KEY="00112233445566778899aabbccddeeff"
 
 while ! docker exec "$container_name" curl -sf "http://[${BR_IPV6}]:80" > /dev/null ; do
-    # Test with a random string that is outputted when a webpage is returned
-
     sleep 5
-
 	echo "sleeping then trying again"
 done
 
