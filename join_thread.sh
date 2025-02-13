c269144d896a16099c123df7a5e5c0279ebf261c
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-02-13 14:13:40 +0100

    feat: get available networks first

---
+++ join_thread.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+set -exo pipefail
+
+if [[ -z "$1" ]] ; then 
+    echo "Please provide the container name"
+    exit 1
+fi
+
+if [[ -z "$2" ]] ; then 
+    echo "Please provide the border router number"
+    exit 1
+fi
+
+set -u
+
+container_name="$1"
+NUMBER="$2"
+ORM_PREFIX="fd71:666b:b2e1:bfd9::"
+MANAGEMENT_IP="10.42.0.10"
+BR_IPV6="fdbe:8cb7:f64c:${NUMBER}::2"
+passphrase="mystify-vantage-deduct"
+NET_KEY="00112233445566778899aabbccddeeff"
+
+docker exec "$container_name" curl -s -H "Content-Type: application/json" --request GET "http://[${BR_IPV6}]:80/available_network"
