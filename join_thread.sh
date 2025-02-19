093bf1a884eeb962b6c41b61b49fa75304f5edb2
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-02-19 11:55:04 +0100

    feat: allow ipv4 in join_thread

--- join_thread.sh
+++ join_thread.sh
@@ -2,35 +2,58 @@
 
 set -exo pipefail
 
-if [[ -z "$1" ]] ; then 
-    echo "Please provide the container name"
-    exit 1
-fi
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -n | --number)
+            NUMBER="$2"
+            shift 2
+            ;;
+        # -h | --help)
+        #     usage
+        #     exit 0
+        #     ;;
+        -4 | --ipv4)
+            IPversion="4"
+            shift
+            ;;
+        *)
+            # Little ugly, but just catch unkown last argument as name            
+            container_name="$1"
+            shift
+            ;;
+    esac
+done
 
-if [[ -z "$2" ]] ; then 
-    echo "Please provide the border router number"
+if [[ "$NUMBER" -lt "1" ]] ; then
+    echo >&2 "Please provide a valid number with -n or --number"
     exit 1
 fi
 
 set -u
 
-container_name="$1"
-NUMBER="$2"
-ORM_PREFIX="fd71:666b:b2e1:bfd9::"
-MANAGEMENT_IP="10.42.0.10"
-BR_IPV6="fdbe:8cb7:f64c:${NUMBER}::2"
 passphrase="mystify-vantage-deduct"
 NET_KEY="00112233445566778899aabbccddeeff"
 
-if docker exec "$container_name" curl -s -H "Content-Type: application/json" --request GET "http://[${BR_IPV6}]:80/available_network" | grep -q "\"error\":0" ; then 
+ORM_PREFIX="fd71:666b:b2e1:bfd9::"
+BR_IP="fdbe:8cb7:f64c:${NUMBER}::2"
+url="http://[${BR_IP}]:80"
+
+if [[ "$IPversion" = "4" ]] ; then
+    BR_IP="10.43.${NUMBER}.2"
+    url="http://${BR_IP}:80"
+fi
+
+set -u
+
+if docker exec "$container_name" curl -s -H "Content-Type: application/json" --request GET "${url}/available_network" | grep -q "\"error\":0" ; then 
     docker exec "$container_name" curl -s -H "Content-Type: application/json" --request POST --data '{
         "credentialType":"networkKeyType",
         "networkKey":"00112233445566778899aabbccddeeff",
-        "prefix":"fd11:22::1:1",
+        "prefix":"fd11:22:1:1",
         "defaultRoute":false,
         "index":0
     }' \
-     "http://[${BR_IPV6}]:80/join_network"
+     "${url}/join_network"
 else
     echo "Failed to get available networks"
     exit 1
