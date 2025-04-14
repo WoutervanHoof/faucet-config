7968aadee00e40b29504b30a61bb59ebd6c29b45
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-04-14 16:16:29 +0100

    fix: better formatting

--- run_demo.sh
+++ run_demo.sh
@@ -54,7 +54,17 @@ border_router_up() {
         exit 1
     fi
 
-    docker run -d --name="thread-br" --net=none --sysctl "net.ipv6.conf.all.disable_ipv6=0 net.ipv4.conf.all.forwarding=1 net.ipv6.conf.all.forwarding=1" -p 8080:80 --dns=172.0.0.1 -it --volume /dev/ttyACM0:/dev/ttyACM0 --volume "$dumps_dir":"$dumps_dir" --privileged openthread/otbr --radio-url spinel+hdlc+uart:///dev/ttyACM0
+    docker run -d --name="thread-br" --net=none \
+	--sysctl "net.ipv6.conf.all.disable_ipv6=0" \
+	--sysctl "net.ipv4.conf.all.forwarding=1" \
+	--sysctl "net.ipv6.conf.all.forwarding=1" \
+	-p 8080:80 --dns=172.0.0.1 -it \
+	--volume /dev/ttyACM0:/dev/ttyACM0 \
+	--volume "$dumps_dir":"$dumps_dir" \
+	--privileged \
+	wouter/otbr-pi \
+	--radio-url "spinel+hdlc+uart:///dev/ttyACM0?uart-baudrate=115200&uart-flow-control"
+>>>>>>> b89b0cd (fix: better formatting)
 
     ovs-docker add-port "$BRIDGE" eth0 thread-br --ipaddress="$BORDER_ROUTER_SUBNET" --gateway="$BRIDGE_IP"
 }
