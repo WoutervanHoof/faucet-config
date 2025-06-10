ac4e4951a2937f30f1ac70c8a6d9c58065c3020d
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-06-10 10:35:12 +0200

    fix: more bash syntax

--- scripts/experiments/exp1.sh
+++ scripts/experiments/exp1.sh
@@ -8,9 +8,9 @@ iterations=1000
 server_success=0
 attacker_success=0
 
-for i in {1..$iterations} ; do 
+for i in {1.."$iterations"} ; do 
     if docker exec server nc -z -w 1 "$CHILD_IP" ; 
-        ((server_success = server_success + 1))
+        ((server_success=server_success+1))
     fi
 done
 ((server_failures = iterations - server_success))
@@ -19,13 +19,13 @@ done
 echo "Number of successful connections to the server:   $server_success"
 echo "Number of unsuccessful connections to the server: $server_failures"
 
-for i in {1..$iterations} ; do 
+for i in {1.."$iterations"} ; do 
     if docker exec attacker nc -z -w 1 "$CHILD_IP" ; 
-        ((attacker_success = attacker_success + 1))
+        ((attacker_success=attacker_success+1))
     fi
 done
 
-((attacker_failures = iterations - attacker_success))
+((attacker_failures=iterations-attacker_success))
 
 
 echo "Number of successful connections to the attacker:   $attacker_success"
