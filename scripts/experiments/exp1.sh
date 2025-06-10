b8bbe5814bc5abfd090fe74a69468f419d22986c
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-06-10 10:35:57 +0200

    fix: forgot the then

--- scripts/experiments/exp1.sh
+++ scripts/experiments/exp1.sh
@@ -9,7 +9,7 @@ server_success=0
 attacker_success=0
 
 for i in {1.."$iterations"} ; do 
-    if docker exec server nc -z -w 1 "$CHILD_IP" ; 
+    if docker exec server nc -z -w 1 "$CHILD_IP" ; then
         ((server_success=server_success+1))
     fi
 done
@@ -20,7 +20,7 @@ echo "Number of successful connections to the server:   $server_success"
 echo "Number of unsuccessful connections to the server: $server_failures"
 
 for i in {1.."$iterations"} ; do 
-    if docker exec attacker nc -z -w 1 "$CHILD_IP" ; 
+    if docker exec attacker nc -z -w 1 "$CHILD_IP" ; then
         ((attacker_success=attacker_success+1))
     fi
 done
