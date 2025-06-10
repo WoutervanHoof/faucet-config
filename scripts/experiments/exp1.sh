bef082eee3f858eca5aca133a802fc4d38e787dd
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-06-10 10:00:03 +0100

    fix: script exited when result of sum was 0...

--- scripts/experiments/exp1.sh
+++ scripts/experiments/exp1.sh
@@ -2,9 +2,8 @@
 
 set -eo pipefail
 
-source ./network_layout.sh
-
-iterations=1000
+iterations=5
+timeout=0.5
 server_success=0
 attacker_success=0
 CHILD_IP="fd71:666b:b2e1:bfd9:44ea:9c0a:d388:64ae"
@@ -15,11 +14,12 @@ echo ""
 
 for i in $(seq "$iterations") ; do 
     if docker exec server nc -z -w 1 "$CHILD_IP" 23 ; then
-        ((server_success=server_success+1))
+        ((server_success+=1))
     fi
+    sleep "$timeout"
 done
-((server_failures = iterations - server_success))
 
+server_failures=$((iterations-server_success))
 
 echo "Number of successful connections to the server:   $server_success"
 echo "Number of unsuccessful connections to the server: $server_failures"
@@ -30,12 +30,12 @@ echo ""
 
 for i in $(seq "$iterations") ; do 
     if docker exec attacker nc -z -w 1 "$CHILD_IP" 23 ; then
-        ((attacker_success=attacker_success+1))
+        ((attacker_success+=1))
     fi
+    sleep "$timeout"
 done
 
-((attacker_failures=iterations-attacker_success))
-
+attacker_failures=$((iterations-attacker_success))
 
 echo "Number of successful connections to the attacker:   $attacker_success"
 echo "Number of unsuccessful connections to the attacker: $attacker_failures"
