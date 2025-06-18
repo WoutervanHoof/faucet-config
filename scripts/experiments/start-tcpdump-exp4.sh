f9b0801617c927697a530e86bba86727a502b6fa
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-06-18 21:48:29 +0200

    fix: redirect stdout to file

--- scripts/experiments/start-tcpdump-exp4.sh
+++ scripts/experiments/start-tcpdump-exp4.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo rm -f nohup.out
-sudo nohup tcpdump -i wlan0 -w "/usr/share/dumps/exp4/single_$(date +%F_%T).pcap" &
+sudo nohup tcpdump -i wlan0 -w "/usr/share/dumps/exp4/single_$(date +%F_%T).pcap" >> /home/wouter/tcpdump$(date +%F_%T).log 2>&1 &
 
 # Write tcpdump's PID to a file
 sudo echo $! > /var/run/tcpdump.pid
