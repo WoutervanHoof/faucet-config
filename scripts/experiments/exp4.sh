f4eb3aa8a08dc10c3b4a8503fced04d655646657
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-06-23 10:49:36 +0200

    fix: parameterize dump file name, test for role change instead of long timeout

--- scripts/experiments/exp4.sh
+++ scripts/experiments/exp4.sh
@@ -4,9 +4,9 @@ timeout 1500s
 
 set a 0
 loop:
-if a = 100 goto finish
+if a = 5 goto finish
 ! ssh wouter@10.42.0.10 -- sudo /home/wouter/faucet-config/scripts/experiments/stop-tcpdump-exp4.sh
-! ssh wouter@10.42.0.170 -- sudo /home/wouter/faucet-config/scripts/experiments/stop-tcpdump-exp4.sh
+# ! ssh wouter@10.42.0.170 -- sudo /home/wouter/faucet-config/scripts/experiments/stop-tcpdump-exp4.sh
 ! /home/wouter/projects/thesis/faucet-config/scripts/reset_demo.sh
 
 inc a
@@ -17,8 +17,8 @@ expect {
 }
 
 ifconfig:
-! ssh wouter@10.42.0.10 -- sudo /home/wouter/faucet-config/scripts/experiments/start-tcpdump-exp4.sh
-! ssh wouter@10.42.0.170 -- sudo /home/wouter/faucet-config/scripts/experiments/start-tcpdump-exp4.sh
+! ssh wouter@10.42.0.10 -- sudo /home/wouter/faucet-config/scripts/experiments/start-tcpdump-exp4.sh test_1
+# ! ssh wouter@10.42.0.170 -- sudo /home/wouter/faucet-config/scripts/experiments/start-tcpdump-exp4.sh double_2
 send ot ifconfig up\n
 expect {
     uart:~$ goto attach
@@ -27,17 +27,29 @@ expect {
 attach:
 send ot thread start\n
 expect {
-    uart:~$ goto detach
+    uart:~$ goto test_state
+}
+
+test_state:
+sleep 2
+send ot state\n
+expect {
+    detached goto test_state
+    child goto detach
+    timeout 10 goto error
 }
 
 detach:
-sleep 10
-
+sleep 1
 send ot detach\n
 expect {
     Done goto loop
 }
 
-
 finish:
 print "Done!"
+exit 0
+
+error:
+print "Something went wrong"
+exit 1
--- scripts/experiments/start-tcpdump-exp4.sh
+++ scripts/experiments/start-tcpdump-exp4.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+if [[ $# != 1 ]] ; then
+    echo "missing required argument. Usage: ./start_tcpdump-exp4.sh File_Base_Name"
+fi
+
+filename=$1
+
 sudo rm -f nohup.out
-sudo nohup tcpdump -i wlan0 -w "/usr/share/dumps/exp4/double_$(date +%F_%T).pcap" >> /home/wouter/tcpdump$(date +%F_%T).log 2>&1 &
+sudo nohup tcpdump -i wlan0 -w "/usr/share/dumps/exp4/${filename}_$(date +%F_%T).pcap" >> /home/wouter/tcpdump$(date +%F_%T).log 2>&1 &
 
 # Write tcpdump's PID to a file
 sudo echo $! > /home/wouter/tcpdump.pid
